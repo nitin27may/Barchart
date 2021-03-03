955a47aef7bea1db8322d7ba71224f5285ce0089
nitin27may <nitin27may@gmail.com>
2021-03-03 13:21:04 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# swimlane-stacked-vertical-bar-chart-xhu2dq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/swimlane-stacked-vertical-bar-chart-xhu2dq)
---
+++ app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { multi } from './data';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  multi: any[];
+  view: any[] = [700, 400];
+
+  // options
+  showXAxis: boolean = true;
+  showYAxis: boolean = true;
+  gradient: boolean = false;
+  showLegend: boolean = true;
+  showXAxisLabel: boolean = true;
+  xAxisLabel: string = 'Country';
+  showYAxisLabel: boolean = true;
+  yAxisLabel: string = 'Population';
+  animations: boolean = true;
+
+  colorScheme = {
+    domain: ['#5AA454', '#C7B42C', '#AAAAAA']
+  };
+
+  constructor() {
+    Object.assign(this, { multi });
+  }
+
+  onSelect(event) {
+    console.log(event);
+  }
+}
